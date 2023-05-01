4085757d7d6649c1b630f8b3a9381c95f5fbd673
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2023-05-01 12:38:08 +0530

    animation added

---
+++ assets/js/about.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   $('.statistics-slider').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      vertical: true,
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      pauseOnDotsHover: false
+   });
+})
