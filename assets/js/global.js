07b7deb2240bd8b7c984f951233cf509b6c2d91c
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2023-05-21 10:15:03 +0530

    index optimization

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,16 @@
+// Passive event listeners
+jQuery.event.special.touchstart = {
+   setup: function (_, ns, handle) {
+      this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+   }
+};
+jQuery.event.special.touchmove = {
+   setup: function (_, ns, handle) {
+      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+   }
+};
+
+// Handling Scoll
 $(window).scroll(function () {
    if ($(this).scrollTop() > 80) {
       $('#header').addClass('scroll');
